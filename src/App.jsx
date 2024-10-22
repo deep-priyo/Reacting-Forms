c54d2c21dcaf053b9724af95fc360c2d6e4971f4
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-22 21:35:44 +0530

    learnt ReactHookForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import ControlledComponents from "./components/ControlledComponents";
 import ExampleComponent from "./components/ExampleComponent";
+import ReactHookForm from "./components/ReactHookForm";
 import UseRef from "./components/useRef";
 
 const App = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center bg-gray-100">
-      <h1 className="text-orange-600 text-4xl font-bold mb-5">useRef Example</h1>
+      {/* <h1 className="text-orange-600 text-4xl font-bold mb-5">useRef Example</h1>
       <UseRef />
       <h1 className="text-orange-600 text-4xl font-bold mt-10 mb-5">Controlled Components</h1>
-      <ControlledComponents />
+      <ControlledComponents /> */}
+      <h1 className="text-orange-600 text-4xl font-bold mt-10 mb-5">React Hook Form</h1>
+      <ReactHookForm />
     </div>
   );
 };
---
+++ src/components/ReactHookForm.jsx
@@ -0,0 +1,17 @@
+import { useForm } from "react-hook-form";
+import styles from "./Custom.module.css"
+const ReactHookForm = () => {
+    const { register, handleSubmit } = useForm();
+    console.log(register());
+    return (
+        <div>
+            <form action="" onSubmit={handleSubmit(data => console.log(data))}>
+                <input {...register('name')} type="text" placeholder="name" className={styles.input} />
+                <input {...register('email')} type="text" placeholder="email" className={styles.input} />
+                <input type="submit" className={styles.submit} />
+            </form>
+        </div>
+    );
+};
+
+export default ReactHookForm;
