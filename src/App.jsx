ae251f2094b43a28c4a2f2c353742afd9af1887f
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-22 19:37:45 +0530

    improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import ControlledComponents from "./components/ControlledComponents";
 import ExampleComponent from "./components/ExampleComponent";
 import UseRef from "./components/useRef";
+
 const App = () => {
   return (
-    <div>
-    
+    <div className="flex flex-col items-center justify-center h-screen text-center bg-gray-100">
+      <h1 className="text-orange-600 text-4xl font-bold mb-5">useRef Example</h1>
+      <UseRef />
+      <h1 className="text-orange-600 text-4xl font-bold mt-10 mb-5">Controlled Components</h1>
       <ControlledComponents />
-    </div>);
+    </div>
+  );
 };
+
 export default App;
--- src/components/ControlledComponents.jsx
+++ src/components/ControlledComponents.jsx
@@ -6,10 +6,11 @@ const ControlledComponents = () => {
     return (
         <div className="" action="" onSubmit={preventSubmit}>
             <form >
-                <input onChange={(event) => setVal({ name: event.target.value })} type="text" placeholder="name" />
-                <input type="submit" className="px-2 py-3 bg-blue-400 text-white text-xs rounded-full" />
+                <input onChange={(event) => setVal({ name: event.target.value })} type="text" placeholder="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 mt-2 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                <input type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 m-3 dark:hover:bg-blue-700 dark:focus:ring-blue-800" />
             </form>
-            <h1>You typed: {val.name}</h1>
+            <h1 className="font-semibold">You typed:
+                <span className="font-light text-teal-600	">{val.name}</span></h1>
         </div>
     );
 };
