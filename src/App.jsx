9bb492b523a08c4eb869f754313dc65a45f20031
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-22 22:09:33 +0530

    several changes made

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import UseRef from "./components/useRef";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center bg-gray-100">
+    <div className=" bg-zinc-400 flex flex-col items-center justify-center h-screen text-center bg-gray-100">
       {/* <h1 className="text-orange-600 text-4xl font-bold mb-5">useRef Example</h1>
       <UseRef />
       <h1 className="text-orange-600 text-4xl font-bold mt-10 mb-5">Controlled Components</h1>
--- src/components/ReactHookForm.jsx
+++ src/components/ReactHookForm.jsx
@@ -6,7 +6,7 @@ const ReactHookForm = () => {
     return (
         <div>
             <form action="" onSubmit={handleSubmit(data => console.log(data))}>
-                <input {...register('name')} type="text" placeholder="name" className={styles.input} />
+                <input {...register('name',{required:true})} type="text" placeholder="name" className={styles.input} />
                 <input {...register('email')} type="text" placeholder="email" className={styles.input} />
                 <input type="submit" className={styles.submit} />
             </form>
